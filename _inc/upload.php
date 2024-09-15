052b3f138d0760808c214266cc457d564eca9b9f
Dev Shawon <islamshawon1993@gmail.com>
2024-09-16 04:32:56 +0600

    Update upload.php

--- _inc/upload.php
+++ _inc/upload.php
@@ -3,7 +3,7 @@ include 'connect.php';
 $target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-    $conn->query("insert into fonts (id,font_name,group_id) values (null,'".$_FILES["file"]["name"]."',null)");
+    $conn->query("insert into fonts (id,font_name) values (null,'".$_FILES["file"]["name"]."')");
     echo "The file ". htmlspecialchars( basename( $_FILES["file"]["name"])). " has been uploaded.";
 } else {
     echo "Sorry, there was an error uploading your file.";
